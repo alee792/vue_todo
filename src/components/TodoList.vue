621da864e1eb8e727b8719d5fc970ac8768476f5
Anthony Lee <alee792@gmail.com>
2017-10-18 15:20:30 -0400

    Add bind key for delete

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,6 +3,7 @@
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
     <todo v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.id"></todo>
+    <todo  v-on:delete-todo="deleteTodo" v-for="todo in todos"v-bind:key="todo.id" v-bind:todo="todo"></todo>
 
   </div>
 </template>
