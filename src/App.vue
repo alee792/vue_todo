5a3c513d494783a1047a44e428c53f6edb237d27
Anthony Lee <alee792@gmail.com>
2017-10-18 13:07:22 -0400

    Create data object

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <todo-list></todo-list>
   </div>
 </template>
 
@@ -12,6 +11,27 @@ export default {
   name: 'app',
   components: {
     TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
   },
 };
 </script>
